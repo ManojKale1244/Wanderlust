de328c67fec795b510dd423664b4e2950d462b15
Manoj  Kale <manojkale187@gmail.com>
2025-11-01 23:03:04 +0530

    Add review schema to schema.js

--- schema.js
+++ schema.js
@@ -7,7 +7,14 @@ module.exports.listingSchema = Joi.object({
     location: Joi.string().required(),
     country: Joi.string().required(),
     price: Joi.number().required().min(0),
-    image: Joi.any(),          // ✅ allow file upload field
-    imageUrl: Joi.string().allow("", null) // ✅ optional URL field
+    image: Joi.any(),          // allow file upload field
+    imageUrl: Joi.string().allow("", null) // optional URL field
+  }).required()
+});
+
+module.exports.reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    comment: Joi.string().required()
   }).required()
 });
